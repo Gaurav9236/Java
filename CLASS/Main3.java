6df3b63a505d349c23d920aa4d0a30f812a20934
Gaurav9236 <enggaurav.eg@gmail.com>
2026-02-06 09:57:53 +0530

    Main3.java

---
+++ CLASS/Main3.java
@@ -0,0 +1,11 @@
+class StaticMethod{
+    static void print(){
+        System.out.println( "I am a static method");
+    }
+}
+public class Main3 {
+    public static void main(String[] args) {
+        StaticMethod.print();
+    }
+    
+}
